79fc41808eb40a82a5a69d6b8c4e280033d4da80
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 21:07:18 +0300

    fix new problem : N_Char

---
+++ CodeforcesAssiutSheets/NewCommers/N_Char.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N
+    class N_Char
+    {
+        public N_Char()
+        {
+            //Capital Letters(A–Z): 65–90
+            //Small Letters(a–z): 97–122
+
+            string userInput = Console.ReadLine() ?? string.Empty;
+            char letter = char.Parse(userInput);
+
+            char result = ' ';
+            if (letter >= 65 && letter <= 90)
+                result = (char)(letter + 32);
+
+            else if (letter >= 97 && letter <= 122)
+                result = (char)(letter - 32);
+
+            Console.WriteLine(result);
+        }
+    }
+}
