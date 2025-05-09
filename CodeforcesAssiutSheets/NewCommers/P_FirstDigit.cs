b4d27306a798de5fed4452fe79bdf7f75619ff62
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 22:04:57 +0300

    fix new problem P_FirstDigit

---
+++ CodeforcesAssiutSheets/NewCommers/P_FirstDigit.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/P
+    class P_FirstDigit
+    {
+        public P_FirstDigit()
+        {
+            string userInput = Console.ReadLine() ?? string.Empty;
+            int.TryParse(userInput[0].ToString(), out int n);
+
+            var reminder = n % 2;
+
+            Console.WriteLine(reminder == 0 ? "EVEN" : "ODD");
+        }
+    }
+}
