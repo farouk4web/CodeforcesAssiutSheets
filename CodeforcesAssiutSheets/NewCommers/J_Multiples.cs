741199ae74a58f928b75914f13daa3b50cf1fe75
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 19:23:59 +0300

    fix new problem : J_Multiples

---
+++ CodeforcesAssiutSheets/NewCommers/J_Multiples.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+    class J_Multiples
+    {
+        public J_Multiples()
+        {
+            var userInput = Console.ReadLine() ?? string.Empty;
+            int[] ints = userInput.Split(' ').Select(int.Parse).ToArray();
+            int noA = ints[0],
+                noB = ints[1];
+
+            int remainder = noA % noB;
+            if (remainder != 0)
+                remainder = noB % noA;
+
+            string msg = remainder == 0 ? "Multiples" : "No Multiples";
+            Console.WriteLine(msg);
+        }
+    }
+}
