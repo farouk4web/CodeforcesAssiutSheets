320427432cd27382a76cadaaf7fddd8e83e9ec44
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 19:39:01 +0300

    fix new problem : L_TheBrothers

---
+++ CodeforcesAssiutSheets/NewCommers/L_TheBrothers.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+    class L_TheBrothers
+    {
+        public L_TheBrothers()
+        {
+            string firstUserName = Console.ReadLine() ?? string.Empty;
+            string secondUserName = Console.ReadLine() ?? string.Empty;
+
+            string firstUserFatherName = firstUserName.Split(' ')[1];
+            string secondUserFatherName = secondUserName.Split(' ')[1];
+
+            string result = firstUserFatherName == secondUserFatherName ? "ARE Brothers" : "NOT";
+            Console.WriteLine($"{result}");
+        }
+    }
+}
