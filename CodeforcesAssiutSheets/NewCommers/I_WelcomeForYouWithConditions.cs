15d99d24908a522062577bec190dcbbb8314bbfd
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 19:15:04 +0300

    fix new Problem:  I_WelcomeForYouWithConditions

---
+++ CodeforcesAssiutSheets/NewCommers/I_WelcomeForYouWithConditions.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+    class I_WelcomeForYouWithConditions
+    {
+        public I_WelcomeForYouWithConditions()
+        {
+            var userInput = Console.ReadLine() ?? string.Empty;
+            byte[] ints = userInput.Split(' ').Select(byte.Parse).ToArray();
+            int noA = ints[0],
+                noB = ints[1];
+
+            string msg = noA >= noB ? "Yes" : "No";
+            Console.WriteLine(msg);
+        }
+    }
+}
