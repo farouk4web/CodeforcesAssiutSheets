237255bcdac94fe896292f71d2f661180e6a6b37
Farouk AbdelHamid <103667035+farouk4web@users.noreply.github.com>
2025-05-09 19:53:06 +0300

    fix new Problem :  M_CapitalOrSmallOrDigit

---
+++ CodeforcesAssiutSheets/NewCommers/M_CapitalOrSmallOrDigit.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeforcesAssiutSheets.NewCommers
+{
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+    class M_CapitalOrSmallOrDigit
+    {
+        public M_CapitalOrSmallOrDigit()
+        {
+            //Digits(0–9): 48–57
+            //Capital Letters(A–Z): 65–90
+            //Small Letters(a–z): 97–122
+
+            string userInput = Console.ReadLine() ?? string.Empty;
+            char letter = char.Parse(userInput);
+
+            if (letter >= 48 && letter <= 57)
+            {
+                Console.WriteLine("IS DIGIT");
+            }
+            else if (letter >= 65 && letter <= 90)
+            {
+                Console.WriteLine("ALPHA");
+                Console.WriteLine("IS CAPITAL");
+            }
+            else if (letter >= 97 && letter <= 122)
+            {
+                Console.WriteLine("ALPHA");
+                Console.WriteLine("IS SMALL");
+            }
+        }
+    }
+}
